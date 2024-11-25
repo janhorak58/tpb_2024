a49a2e9105acf30194a2f17a2f0e5c9127395371
Jan Horak <janhorak58@gmail.com>
2024-11-25 12:42:19 +0100

    cv07

---
+++ cv07/words_in_files.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+import time
+
+# Inicializace SparkSession
+spark = SparkSession.builder \
+    .appName("CountWordsFiles").getOrCreate()
+
+accessLines = spark.readStream.text("/files/data")
+
+words = accessLines.select(F.explode(F.split(F.lower(accessLines.value), " ")).alias("word"))
+
+wordsMalaPismenaBezInterpunkce = words. \
+    withColumn("word", F.lower(F.regexp_replace("word", "[^a-z]", "")))
+wordsWithTimestamp = wordsMalaPismenaBezInterpunkce.withColumn("eventTime", F.current_timestamp()) \
+    .withColumn("eventTime", F.date_format("eventTime", "yyyy-MM-dd HH:mm:ss"))
+wordCounts = wordsWithTimestamp.groupBy(
+    F.window(F.col("eventTime"), "30 seconds", "15 seconds"),
+    F.col("word")
+    ).count()
+
+
+
+sortedWordCounts = wordCounts.orderBy("count", ascending=False) \
+    .withColumn("timestamp", F.date_format(F.col("window.start"), "HH:mm:ss"))
+
+query = sortedWordCounts.writeStream \
+    .outputMode("complete") \
+    .format("console") \
+    .queryName("word_counts") \
+    .start()
+    
+query.awaitTermination()
+
+while True:
+    spark.sql("SELECT * FROM word_counts").show()
+    time.sleep(5)
+    
+    
